c790e38021b5f3f34a0d29d436b0ebd4d8c393ec
Bruno Dietrich <dietrich.bruno@gmail.com>
2023-06-26 17:32:14 -0300

    Signup logic

--- makedown.sh
+++ makedown.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 kubectl delete svc ingress-nginx-controller -n ingress-nginx &&
-kubectl delete svc ingress-nginx-controller-admission -n ingress-nginx
+kubectl delete svc ingress-nginx-controller-admission -n ingress-nginx &&
+kubectl delete deploy ingress-nginx-controller -n ingress-nginx
--- makeup.sh
+++ makeup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.0/deploy/static/provider/cloud/deploy.yaml &&
 
-skaffold dev
+$ skaffold dev --no-prune=false --cache-artifacts=false --no-prune-children=false
